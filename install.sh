214c9432640b0f022578d7ca521da0e8bc6cc114
Richard Käll <richard.kall@me.com>
2013-06-16 11:16:39 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -7,24 +7,20 @@ DIR=~/.dotfiles
 BACKUP_DIR=~/.dotfiles_old
 
 # List of files & folders to symlink
-FILES=".gemrc .vim .vimrc .vimrc.bundles .zshrc .zshenv .gitconfig .powconfig .tmuxinator .tmux.conf .oh-my-zsh/themes/richard.zsh-theme .ssh"
+FILES=".gemrc .gitconfig .tmux.conf .vim .vimrc .vimrc.bundles .zsh .zshrc"
 
 # Create backup folder
 echo "Creating $BACKUP_DIR for backup of any existing dotfiles..."
 mkdir -p $BACKUP_DIR
-echo "...done!"
 
 # Change to the dotfiles folder
 echo "Changing to the $DIR folder..."
 cd $DIR
-echo "...done!"
 
 # Move/backup existing dotfiles then create symlinks
 for FILE in $FILES; do
   echo "Backing up $FILE from ~ to $BACKUP_DIR..."
   mv ~/$FILE $BACKUP_DIR
-  echo "...done!"
   echo "Creating symlink to $FILE in home directory..."
   ln -s $DIR/$FILE ~/$FILE
-  echo "...done!"
 done
